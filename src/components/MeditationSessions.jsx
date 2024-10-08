caaba33fb1164227577728c367603feb57c6b708
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-09 02:27:30 +0800

    render data

--- src/components/MeditationSessions.jsx
+++ src/components/MeditationSessions.jsx
@@ -15,7 +15,31 @@ function MeditationSessions() {
     return (
         <div>
             <h1>Meditation Sessions</h1>
-            {JSON.stringify(sessions)}
+            <div>
+
+                <table>
+                    <thead>
+                        <th>Description</th>
+                        <th>YouTube Title</th>
+                        <th>Finish Time</th>
+                        <th>Length</th>
+                    </thead>
+                    <tbody>
+                        {sessions.map(session => {
+                            const { _id, description, finishTime, youtubeTitle, length } = session
+                            return (<tr key={_id}>
+                                <td>{description}</td>
+                                <td>{youtubeTitle}</td>
+                                <td>{finishTime}</td>
+                                <td>{length}</td>
+                            </tr>)
+                        }
+                        )}
+                    </tbody>
+                </table>
+
+
+            </div>
         </div>
     )
 }
