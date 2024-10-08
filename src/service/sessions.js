5d394cca69bfd147332682d68e96e362e1004600
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-09 05:09:53 +0800

    descending order

--- src/service/sessions.js
+++ src/service/sessions.js
@@ -6,7 +6,7 @@ const BASE_URL = 'http://localhost:5000'
 export const getSessions = async () => {
     try {
         const sessions = await axios.get(`${BASE_URL}/sessions`)
-        return sessions.data
+        return sessions.data.sort((a, b) => b.finishTime - a.finishTime)
     } catch (error) {
         console.error(error)
         return []
