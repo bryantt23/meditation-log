4e5e77a8106ed2006c7d5cec1becec23120c0bdd
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-18 00:32:56 -0700

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
