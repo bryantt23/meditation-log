a576e86ef5899398324927a73556d0ba71dd7f74
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-22 03:05:06 -0700

    use cloud backend

--- frontend/src/service/sessions.js
+++ frontend/src/service/sessions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import iso8601Duration from 'iso8601-duration'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 export const getSessions = async () => {
     try {
